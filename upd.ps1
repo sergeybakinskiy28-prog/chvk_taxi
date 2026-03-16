93f735a337f3000b58ddef479b8e56a6869996f0
Sergey <sergeybakinskiy28@gmail.com>
2026-03-17 02:31:32 +0400

    update

--- upd.ps1
+++ upd.ps1
@@ -5,8 +5,11 @@ git add .
 git commit -m $msg
 git push origin main
 
-# 2. Запуск из правильной папки
-Write-Host "--- STARTING BOT FROM BACKEND ---" -ForegroundColor Cyan
+# 2. Настройка путей и запуск
+Write-Host "--- STARTING BOT ---" -ForegroundColor Cyan
 
-# Используем найденный путь
+# Указываем Python, что корень проекта здесь
+$env:PYTHONPATH = "."
+
+# Запускаем как модуль (через точку), это решит проблему с импортами
 python chvk_city/backend/main.py
