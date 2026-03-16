2c99c0337fe3a503901f2489866f3f65693d9fe0
Sergey <sergeybakinskiy28@gmail.com>
2026-03-17 02:32:10 +0400

    update

--- upd.ps1
+++ upd.ps1
@@ -1,15 +1,13 @@
 param($msg = "update")
 
-# 1. Сохранение
+# 1. Гит
 git add .
 git commit -m $msg
 git push origin main
 
-# 2. Настройка путей и запуск
+# 2. Запуск
 Write-Host "--- STARTING BOT ---" -ForegroundColor Cyan
-
-# Указываем Python, что корень проекта здесь
 $env:PYTHONPATH = "."
 
-# Запускаем как модуль (через точку), это решит проблему с импортами
+# Запускаем так, чтобы процесс не обрывался
 python chvk_city/backend/main.py
